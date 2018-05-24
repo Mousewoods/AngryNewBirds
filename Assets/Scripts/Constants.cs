85ca216bb19a51fd908343872176e03867fc2efd
Loïc Mauvoisin <loic.mauvoisin@edu.itescia.fr>
2018-05-24 15:33:19 +0200

    firstCommit

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class Constants {
+
+	//Musics
+	public static int MENU_MUSIC = 0;
+	public static int GAMEPLAY_MUSIC = 1;
+
+	//Sfx
+	public static string GAMEOVER_SFX = "GameOver";
+    public static string VICTOIRE_SFX = "Victoire";
+    public static string LANCER_SFX = "Lancer";
+    public static string PAF_SFX = "Paf";
+    public static string EXPLOSION_SFX = "Explosion";
+    public static string PULL_SFX = "Pull";
+
+}
